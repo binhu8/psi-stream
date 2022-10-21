7827f936e9e26263571bb3da0fa980a46205e29d
Fabio Silva <binhudo8@gmail.com>
2022-10-21 14:54:42 -0300

    cors allow

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
-const {  v4: uuidv4} = require('uuid')
+const {  v4: uuidv4} = require('uuid');
+const cors = require('cors')
 
+
+app.use(cors())
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res)=> {
-    res.redirect(`/${uuidv4()}`)
+    res.send(`https://psi-stream.herokuapp.com/${uuidv4()}`)
 });
 
 app.get('/:room', (req, res)=> {
